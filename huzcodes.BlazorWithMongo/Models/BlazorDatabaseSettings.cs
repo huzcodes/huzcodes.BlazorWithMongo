87fb7728881c630baf03c4a2c78519c9cdea384d
huzcodes <huzcodes@outlook.com>
2024-02-26 00:04:45 +0400

    adding project files and service implementation and docker configuration

---
+++ huzcodes.BlazorWithMongo/Models/BlazorDatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace huzcodes.BlazorWithMongo.Models
+{
+    public class BlazorDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string BooksCollectionName { get; set; } = null!;
+    }
+}
---
+++ huzcodes.BlazorWithMongo/Program.cs
@@ -0,0 +1,30 @@
+using huzcodes.BlazorWithMongo.Components;
+using huzcodes.BlazorWithMongo.Models;
+using huzcodes.BlazorWithMongo.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.Configure<BlazorDatabaseSettings>(
+    builder.Configuration.GetSection("BlazorDatabase"));
+
+builder.Services.AddSingleton<BooksService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
