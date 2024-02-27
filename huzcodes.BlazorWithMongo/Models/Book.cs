ca46af0b332078df52f93df419c5d5ee9d16e943
huzcodes <huzcodes@outlook.com>
2024-02-28 00:23:50 +0400

    updating the document model by json attributes to match the ones created on mongo

--- huzcodes.BlazorWithMongo/Models/Book.cs
+++ huzcodes.BlazorWithMongo/Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson;
+using Newtonsoft.Json;
 
 namespace huzcodes.BlazorWithMongo.Models
 {
@@ -7,15 +8,16 @@ namespace huzcodes.BlazorWithMongo.Models
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
+        [JsonProperty("id")]
         public string? Id { get; set; }
 
-        [BsonElement("Name")]
+        [JsonProperty("bookName")]
         public string BookName { get; set; } = null!;
-
+        [JsonProperty("price")]
         public decimal Price { get; set; }
-
+        [JsonProperty("categort")]
         public string Category { get; set; } = null!;
-
+        [JsonProperty("author")]
         public string Author { get; set; } = null!;
     }
 }
