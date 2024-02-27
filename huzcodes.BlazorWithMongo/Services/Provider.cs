2481005d723851b4a824ac1b58ab9633db861360
huzcodes <huzcodes@outlook.com>
2024-02-28 00:22:16 +0400

    adding implementation for inserting into mongo with docker

---
+++ huzcodes.BlazorWithMongo/Services/Provider.cs
@@ -0,0 +1,18 @@
+﻿using huzcodes.BlazorWithMongo.Models;
+
+namespace huzcodes.BlazorWithMongo.Services
+{
+    public class Provider(BooksService booksService) : IProvider
+    {
+        private readonly BooksService _booksService = booksService;
+        public async ValueTask Create(int currentCount)
+        {
+            var oBook = new Book();
+            oBook.Author = $"Author {currentCount}";
+            oBook.Price = currentCount * 10;
+            oBook.Category = $"Category {currentCount}";
+            oBook.BookName = $"BookName {currentCount}";
+            await _booksService.CreateAsync(oBook);
+        }
+    }
+}
